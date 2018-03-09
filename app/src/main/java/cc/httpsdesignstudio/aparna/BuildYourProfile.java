e8775568943f162b3708fef959b88c704eed6ec6
Saumia_Rocks <saumiasinghal@gmail.com>
2018-03-09 14:23:36 +0530

    Made changes

--- app/src/main/java/cc/httpsdesignstudio/aparna/BuildYourProfile.java
+++ app/src/main/java/cc/httpsdesignstudio/aparna/BuildYourProfile.java
@@ -3,6 +3,7 @@ package cc.httpsdesignstudio.aparna;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,6 +21,7 @@ public class BuildYourProfile extends AppCompatActivity {
     TextView tvBirthDate;
     Button btnNext;
     Button btnNext2;
+    Button btnNext3;
     ProgressBar progressBar;
 
 
@@ -38,6 +40,7 @@ public class BuildYourProfile extends AppCompatActivity {
         tvBirthDate = findViewById(R.id.picDate);
         btnNext = findViewById(R.id.btnNext);
         btnNext2 = findViewById(R.id.btnNext2);
+        btnNext3 = findViewById(R.id.btnNext3);
         progressBar = findViewById(R.id.progressBar);
 
         progressBar.setProgress(33);
@@ -83,12 +86,21 @@ public class BuildYourProfile extends AppCompatActivity {
                     fragmentTransaction.addToBackStack(null);
                     fragmentTransaction.commit();
 
-                    progressBar.setProgress(33);
+                    progressBar.setProgress(66);
 
                     btnNext.setVisibility(View.GONE);
                     btnNext2.setVisibility(View.VISIBLE);
                 }
 
+
+
+
+            }
+        });
+
+        btnNext2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 if(etSkills.getText().toString().trim()!= null && etEducationQualification.getText().toString().trim() != null) {
                     Log.e("Status", "changing edu to interest");
                     String educationQualification = etEducationQualification.getText().toString();
@@ -105,8 +117,14 @@ public class BuildYourProfile extends AppCompatActivity {
                     fragmentTransaction.addToBackStack(null);
                     fragmentTransaction.commit();
 
-                    progressBar.setProgress(66);
+                    progressBar.setProgress(100);
                 }
+            }
+        });
+
+        btnNext3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
                 if(etInterest.getText().toString()!= null && etWantToLearn.getText().toString() != null) {
                     Log.e("Status", "final");
@@ -114,9 +132,12 @@ public class BuildYourProfile extends AppCompatActivity {
                     String interest = etInterest.getText().toString();
                     String wantToLearn = etWantToLearn.getText().toString();
 
+                    // start Profile Activity
+                    Intent intent = new Intent(BuildYourProfile.this, FeedActivity.class);
+                    startActivity(intent);
+
                     progressBar.setProgress(100);
                 }
-
 
             }
         });
